79cdd829c43e5f1782be84bbfbf950eb4ab006cb
Ishmam Chowdhury <ishmamchowdhurygp@gmail.com>
2021-09-29 14:54:31 +0600

    initialized empty project

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center"> Battleship Project - JS </h1>
+
+<p align="center">To do.</p>
+
+<hr/>
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: "./src/images/favicon.ico",
+    }),
+    new MiniCssExtractPlugin(),
+    new Dotenv(),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
