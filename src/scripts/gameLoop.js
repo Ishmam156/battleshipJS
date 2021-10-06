f75b9a75a120115e459c048d9a29b7e39f6fed2d
Ishmam Chowdhury <ishmamchowdhurygp@gmail.com>
2021-10-06 13:18:44 +0600

    initial gameloop setup completed

--- src/scripts/gameLoop.js
+++ src/scripts/gameLoop.js
@@ -14,15 +14,100 @@
 // Create conditions so that the game ends once one players ships have all been sunk.
 // This function is appropriate for the Game module.
 
-import { gameBoard } from "./gameBoard";
+import { gameBoard, BOARD_SIZE } from "./gameBoard";
 import { player } from "./player";
-import { createShip } from "./ship";
 
 const gameLoop = () => {
-  console.log("starting");
+  const humanBoard = gameBoard();
+  const computerBoard = gameBoard();
 
-    
+  const humanDOMBoard = document.getElementById("humanBoard");
+  const computerDOMBoard = document.getElementById("computerBoard");
 
+  const boardListener = (event, board) => {
+    const coordinate = [
+      Number(event.target.dataset.row),
+      Number(event.target.dataset.column),
+    ];
+
+    const isHit = board.receiveAttack(coordinate);
+    console.log(coordinate);
+    console.log(isHit);
+
+    if (isHit) {
+      event.target.style.background = "red";
+    } else {
+      event.target.style.background = "blue";
+    }
+  };
+
+  humanDOMBoard.addEventListener("click", (event) =>
+    boardListener(event, humanBoard)
+  );
+  computerDOMBoard.addEventListener("click", (event) =>
+    boardListener(event, computerBoard)
+  );
+
+  const humanPlayer = player(humanBoard);
+  const computerPlayer = player(computerBoard);
+
+  humanBoard.placeShip([
+    [1, 1],
+    [1, 2],
+    [1, 3],
+    [1, 4],
+  ]);
+
+  computerBoard.placeShip([
+    [2, 1],
+    [2, 2],
+    [2, 3],
+  ]);
+
+  humanBoard.placeShip([
+    [3, 3],
+    [3, 4],
+    [3, 5],
+  ]);
+
+  computerBoard.placeShip([
+    [5, 2],
+    [6, 2],
+    [7, 2],
+    [8, 2],
+  ]);
+
+  const displayHumanBoard = humanBoard.getBoard();
+
+  for (let outerIndex = 0; outerIndex < BOARD_SIZE; outerIndex++) {
+    for (let innerIndex = 0; innerIndex < BOARD_SIZE; innerIndex++) {
+      const toAdd = document.createElement("div");
+      toAdd.dataset.row = outerIndex;
+      toAdd.dataset.column = innerIndex;
+      const coordStatus = displayHumanBoard[outerIndex][innerIndex];
+      if (coordStatus.hasShip) {
+        toAdd.style.background = "purple";
+      }
+
+      humanDOMBoard.appendChild(toAdd);
+    }
+  }
+
+  const displayComputerBoard = computerBoard.getBoard();
+
+  for (let outerIndex = 0; outerIndex < BOARD_SIZE; outerIndex++) {
+    for (let innerIndex = 0; innerIndex < BOARD_SIZE; innerIndex++) {
+      const toAdd = document.createElement("div");
+      toAdd.dataset.row = outerIndex;
+      toAdd.dataset.column = innerIndex;
+      const coordStatus = displayComputerBoard[outerIndex][innerIndex];
+      if (coordStatus.hasShip) {
+        toAdd.style.background = "purple";
+      }
+
+      computerDOMBoard.appendChild(toAdd);
+    }
+  }
 };
 
 export { gameLoop };
