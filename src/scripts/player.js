249ba3e79b584f377c684266a25a64f40da35b1a
Ishmam Chowdhury <ishmamchowdhurygp@gmail.com>
2021-10-17 17:49:45 +0600

    made computer AI slightly smarter

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -10,27 +10,54 @@ const player = (gameBoard) => {
     Math.floor(Math.random() * BOARD_SIZE),
   ];
 
+  const smartMove = (coords) => {
+    const newCoords = [...coords];
+    const options = [1, -1];
+    let singleCoord = Math.floor(Math.random() * 2);
+    newCoords[singleCoord] =
+      coords[singleCoord] + options[Math.floor(Math.random() * 2)];
+
+    while (newCoords.includes(-1) || newCoords.includes(10)) {
+      newCoords[singleCoord] =
+        coords[singleCoord] + options[Math.floor(Math.random() * 2)];
+    }
+    return newCoords;
+  };
+
   const makeMove = (coords) => {
     enemyBoard.receiveAttack(coords);
     return true;
   };
 
   const makeRandomMove = () => {
-    let move = randomMove();
+    let move;
+    let moveTries = 0;
+    if (lastMoveHit) {
+      move = smartMove(movesMade[movesMade.length - 1]);
+    } else {
+      move = randomMove();
+    }
 
     while (
       movesMade.some(
         (oldMove) => oldMove[0] === move[0] && oldMove[1] === move[1]
       )
     ) {
-      move = randomMove();
+      if (lastMoveHit && moveTries < 6) {
+        move = smartMove(movesMade[movesMade.length - 1]);
+        moveTries++;
+      } else {
+        move = randomMove();
+      }
     }
     movesMade.push(move);
     const isHit = enemyBoard.receiveAttack(move);
     if (isHit) {
       lastMoveHit = true;
+    } else {
+      lastMoveHit = false;
     }
-    // console.log(lastMoveHit);
+
     return move;
   };
 
